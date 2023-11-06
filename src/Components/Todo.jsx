d8d68db3068924101584f2fc507bad477c7360d8
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-11-06 09:44:41 +0000

    add all files

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react" 
+import Form from "./Form"
+import TodoComponent from "./TodoComponent"
+import Footer from "./Footer";
+   
+export default function Todo() {
+ const [todos, setTodos] = useState([]);
+ const completedTodos = todos.filter((todo)=>todo.done).length     
+ const totalTodos = todos.length
+   return (<div>
+       <Form todos={todos} setTodos={setTodos}/>
+       <TodoComponent todos={todos} setTodos={setTodos}/>
+       <Footer completedTodos={completedTodos} totalTodos={totalTodos}/>
+    </div>
+    )
+} 
